05a1211391906b58c236234cdd173c48897783cd
g4b025 <131189252+g4b025@users.noreply.github.com>
2026-09-20 16:52:21 -0600

    Refresh finance app cache

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='gabo-finanzas-v22';
+const CACHE='gabo-finanzas-v23';
 const ASSETS=['./','./manifest.webmanifest','./icon-192.png','./icon-512.png'];
 self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)))});
 self.addEventListener('activate',e=>e.waitUntil(Promise.all([caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))),self.clients.claim()])));
